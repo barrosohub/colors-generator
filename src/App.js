fd29465b9cbd477bac4ab28b58d3a9a8fdefae9c
barrsofilho <barrosofilho.ti@gmail.com>
2021-12-12 03:14:17 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
-function App() {
+const App = () => {
 
 useEffect(() => {
-  document.title = 'Hello React!';
+  document.title = useColor;
 });
 
 
+const [useColor, setColor] = useState('#fff');
+const [useMsg, setMsg] = useState('');
+
+//funcção para mudar a cor de fundo do body aleatoriamente. A cor de fundo do body deve mudar quando a seta do mouse é movida.
+
+const generateColor = () => {
+  const body = document.querySelector('body');
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+  body.style.backgroundColor = '#' + color;
+  setColor((body.style.backgroundColor).toString());
+  setMsg('Generated color: ' + (body.style.backgroundColor).toString());
+}
+
+//function to copy current color to clipboard
+const copyColor = () => {
+    navigator.clipboard.writeText(useColor);
+    setMsg(useColor + ' copied to clipboard!');
+}
+
+
+
+  useEffect(() => {
+    let eventClearMsg = setTimeout(() => {
+      setMsg('');
+    }, 3000);
+    return () => clearTimeout(eventClearMsg);
+  }, [useMsg]);
+
+
+
+
+
   return (
-    <div className="App">
-      <h1>
-        Hello React!
-      </h1>      
-    </div>  
+    <div className="container">
+        <button onClick={generateColor}><strong>Generate Color</strong></button>
+        <button onClick={copyColor}>Copy <strong>{useColor}</strong> to clipboard</button>
+        <p className="msg">{useMsg}</p>
+    </div>
   );
 }
 
